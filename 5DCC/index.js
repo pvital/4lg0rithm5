e139d4826df9463e33712f97509d2eab01729f1b
Paulo Vital <pvital@gmail.com>
2019-10-25 16:01:04 +0200

    5 Day Code Challenge final files.

---
+++ 5DCC/index.js
@@ -0,0 +1,17 @@
+<script>
+    function ingredientsHover() {
+        document.getElementbyId('ingredients').firstElementChild.firstElementChild.style.fontSize = "300%";
+    }
+
+    function ingredientsNormal() {
+        document.getElementbyId('ingredients').firstElementChild.firstElementChild.style.fontSize = "100%";
+    }
+
+    function preparationHover() {
+        document.getElementbyId('preparation').firstElementChild.firstElementChild.style.fontSize = "300%";
+    }
+
+    function preparationNormal() {
+        document.getElementbyId('preparation').firstElementChild.firstElementChild.style.fontSize = "100%";
+    }
+</script>
